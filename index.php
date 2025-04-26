e8bb5b81d30f7dece0362a50a63e4d754c8e65b5
habimana <habimanaernest47@gmail.com>
2025-04-26 10:05:37 +0300

    This Group-7 assignement2

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>GROUP_7</title>
+</head>
+<body>
+<h1>GROUP_7 in worker of web diveloping</h1>
+<p>The SHA256 hash of "groupe_7" is
+347ff2d619a1f6e511410087102dad4e1264767b0a01e7a7fabe662d811ea559</p>
+<pre>
+ASCII ART:
+    ***********
+    **       **
+    **
+    **
+    **
+    **       **
+    ***********
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
